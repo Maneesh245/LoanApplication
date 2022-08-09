c9be07fc73ff95a96b97b58343b3b32e1c855daa
maneesh245 <manishnishad15@gmail.com>
2022-08-09 22:11:58 +0530

    login component

---
+++ LOANAPPLICATION/src/app/login/login.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthenticationService } from '../service/authentication.service';
+import { AuthResponseDto } from '../model/AuthResponseDto';
+import { HttpErrorResponse } from '@angular/common/http';
+// import { first } from 'rxjs/operators';
+
+
+@Component({templateUrl: 'login.component.html'})
+export class LoginComponent implements OnInit {
+  LoginDetails!: FormGroup;
+    role!:string;
+    returnUrl!: string;
+    errorMessage: string = '';
+    showError!: boolean;
+
+    constructor(
+        private formBuilder: FormBuilder,
+        private route: ActivatedRoute,
+        private router: Router,
+        private authenticationService: AuthenticationService,
+       
+    ) {
+       
+    }
+
+    ngOnInit() {
+        this.LoginDetails = this.formBuilder.group({
+            username: [null,Validators.required ],
+            Password: [null,Validators.required]
+          });
+
+        // get return url from route parameters or default to '/'
+        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    }
+
+    // convenience getter for easy access to form fields
+
+
+    Login() {
+        
+
+        // stop here if form is invalid
+        if (this.LoginDetails.invalid) {
+            return;
+        }
+        const credentials = JSON.stringify(this.LoginDetails.value);
+      
+        this.authenticationService.login(this.LoginDetails.value)
+        .subscribe({
+            next: (res:AuthResponseDto) => {
+             if(res.isAuthSuccessful)
+             {
+             localStorage.setItem("Role", res.Role);
+             this.router.navigateByUrl('');
+             }
+             else
+             {
+                this.errorMessage =res.errorMessage;
+                this.showError = true;
+             }
+
+          },
+          error: (err: HttpErrorResponse) => {
+            this.errorMessage = err.message;
+            this.showError = true;
+          }})
+        }
+}
---
+++ LOANAPPLICATION/src/app/service/authentication.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { User } from '../model/user';
+import { environment } from 'src/environments/environment';
+import { AuthResponseDto } from '../model/AuthResponseDto';
+
+
+
+@Injectable({ providedIn: 'root' })
+export class AuthenticationService {
+  private apiURL = "http://localhost:5000/api";
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
+    constructor(private http: HttpClient) {
+ 
+    }
+
+  
+
+    login(post:any): Observable<AuthResponseDto> {
+      
+              return this.http.post<AuthResponseDto>(`${environment.apiUrl}/Login/Aurthenticate`,  JSON.stringify(post), this.httpOptions)
+        .pipe(
+          catchError(this.handleError)
+        )
+      }  
+ 
+
+    logout() {
+        // remove user from local storage to log user out
+        localStorage.removeItem('currentUser');
+     
+    }
+    private handleError(error: HttpErrorResponse) {
+      if (error.status === 0) {
+        // A client-side or network error occurred. Handle it accordingly.
+        console.error('An error occurred:', error.error);
+      } else {
+        // The backend returned an unsuccessful response code.
+        // The response body may contain clues as to what went wrong.
+        console.error(
+          `Backend returned code ${error.status}, body was: `, error.error);
+      }
+      // Return an observable with a user-facing error message.
+      return throwError(() => new Error('Something bad happened; please try again later.'));
+    }
+}
